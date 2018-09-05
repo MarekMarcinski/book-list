778cec7116621f19e3e085cf4378a5f50ae80160
Marek Marciński <marcinski.mar@gmail.com>
2018-09-05 22:09:45 +0200

    starting project

---
+++ src/main/java/org/marcinski/BookListApplication.java
@@ -0,0 +1,12 @@
+package org.marcinski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookListApplication.class, args);
+    }
+}
