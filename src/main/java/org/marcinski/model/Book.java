1de2dd7f009188b5545d2d6529ed4f8259fe9dcd
Marek Marciński <marcinski.mar@gmail.com>
2018-09-05 22:14:48 +0200

    added sipmle pojo class for book model

---
+++ src/main/java/org/marcinski/model/Book.java
@@ -0,0 +1,21 @@
+package org.marcinski.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String author;
+    private String isbn;
+}
