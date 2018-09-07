53cafbad1982b616f1e5d4325f476813967d64df
Marek Marciński <marcinski.mar@gmail.com>
2018-09-07 18:04:28 +0200

    added query for create schema

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,4 @@
+CREATE SCHEMA IF NOT EXISTS `BOOK-LIST`;
 INSERT INTO `book-list`.`book` (`title`, `author`, `isbn`) VALUES ('Harry Potter', 'J.K. Rowling', '978-83-727-8280-9');
 INSERT INTO `book-list`.`book` (`title`, `author`, `isbn`) VALUES ('Outsider', 'Stephen King', '978-83-812-3266-1');
 INSERT INTO `book-list`.`book` (`title`, `author`, `isbn`) VALUES ('W pułapce', 'Magda Stachula', '978-83-240-4796-3');
