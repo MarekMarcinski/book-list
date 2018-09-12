114e088fa59d47daf5c01b481f584c1bb387b4e4
Marek Marciński <marcinski.mar@gmail.com>
2018-09-12 20:23:51 +0200

    save book method added

--- src/main/java/org/marcinski/api/BookEndpoint.java
+++ src/main/java/org/marcinski/api/BookEndpoint.java
@@ -2,9 +2,15 @@ package org.marcinski.api;
 
 import org.marcinski.model.Book;
 import org.marcinski.repository.BookRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -19,5 +25,20 @@ public class BookEndpoint {
     @GetMapping("/api/books")
     public List<Book> getAll(){
         return bookRepository.findAll();
+    }
+
+    @PostMapping("/api/books")
+    public ResponseEntity<?> save(@RequestBody Book book) {
+        if (book.getId() == null) {
+            Book bookToSave = bookRepository.save(book);
+            URI uri = ServletUriComponentsBuilder
+                    .fromCurrentRequest()
+                    .path("/{id}")
+                    .buildAndExpand(bookToSave.getId())
+                    .toUri();
+            return ResponseEntity.created(uri).body(book);
+        }else {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
     }
 }
