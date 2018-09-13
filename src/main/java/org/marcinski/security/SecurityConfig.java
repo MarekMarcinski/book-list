3d91d458d40d848bca851ca8a02483276fe86944
Marek Marciński <marcinski.mar@gmail.com>
2018-09-13 17:48:54 +0200

    configuration class for sprin security with hardcoded 2 users

---
+++ src/main/java/org/marcinski/security/SecurityConfig.java
@@ -0,0 +1,32 @@
+package org.marcinski.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.httpBasic()
+                .and().logout()
+                .and().csrf()
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST)
+                .authenticated();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("marek").password("stronkpass").roles("USER")
+                .and()
+                .withUser("admin").password("admin").roles("USER");
+    }
+}
