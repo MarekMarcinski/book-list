f0c06000a584de2711b1033fe75f56feb3a8d770
Marek Marciński <marcinski.mar@gmail.com>
2018-09-18 19:16:09 +0200

    -service has been supplemented with a constructor with repositories and a encoder. -also 2 methods have been added to find a user and save a new user

--- src/main/java/org/marcinski/service/UserService.java
+++ src/main/java/org/marcinski/service/UserService.java
@@ -1,9 +1,41 @@
 package org.marcinski.service;
 
+import org.marcinski.model.Role;
+import org.marcinski.model.User;
+import org.marcinski.repository.RoleRepository;
+import org.marcinski.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.HashSet;
 
 @Service
 public class UserService {
 
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
+    @Autowired
+    public UserService(UserRepository userRepository,
+                       RoleRepository roleRepository,
+                       BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    public User findUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public void saveUser(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setActive(1);
+        Role userRole = roleRepository.findByRole("USER");
+        user.setRoles(new HashSet<>(Arrays.asList(userRole)));
+        userRepository.save(user);
+    }
 }
