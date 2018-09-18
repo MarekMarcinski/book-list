82766162c76b210b848e88d2a91e65dbdee96739
Marek Marciński <marcinski.mar@gmail.com>
2018-09-18 18:59:52 +0200

    relation many2many with role added

--- src/main/java/org/marcinski/model/User.java
+++ src/main/java/org/marcinski/model/User.java
@@ -19,6 +19,11 @@ public class User {
     private String name;
     private int active;
 
+    @ManyToMany
+    @JoinTable(
+            name = "user_role"
+            ,joinColumns = @JoinColumn(name = "user_id")
+            ,inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles;
 }
 
