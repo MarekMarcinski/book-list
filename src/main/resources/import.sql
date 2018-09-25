d6bdd76b6bdfbdf9021075b25665b34d485c7a3c
Marek Marciński <marcinski.mar@gmail.com>
2018-09-25 07:10:00 +0200

    inserts commented

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO books (`title`, `author`, `isbn`) VALUES ('Harry Potter', 'J.K. Rowling', '978-83-727-8280-9');
-INSERT INTO books (`title`, `author`, `isbn`) VALUES ('Outsider', 'Stephen King', '978-83-812-3266-1');
-INSERT INTO books (`title`, `author`, `isbn`) VALUES ('W pułapce', 'Magda Stachula', '978-83-240-4796-3');
+-- INSERT INTO books (`title`, `author`, `isbn`) VALUES ('Harry Potter', 'J.K. Rowling', '978-83-727-8280-9');
+-- INSERT INTO books (`title`, `author`, `isbn`) VALUES ('Outsider', 'Stephen King', '978-83-812-3266-1');
+-- INSERT INTO books (`title`, `author`, `isbn`) VALUES ('W pułapce', 'Magda Stachula', '978-83-240-4796-3');
